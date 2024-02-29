b6835a7cde7c1c1d12631bd438596a6131d94dbd
Diegiwg <diegiwg@gmail.com>
2024-02-29 00:05:55 -0300

    feat: add cmd calc with add and sub ops

--- examples/hello/main.php
+++ examples/hello/main.php
@@ -2,7 +2,23 @@
 
 require_once("../../pli.php");
 
-// main
+// RUN: php main.php calc add 1 2
 
-$cmd = parse_command_line(implode(" ", $argv));
-print($cmd->stringify());
+$line = implode(" ", $argv);
+$cmd = parse_command_line($line);
+
+//
+if ($cmd->name === "calc") {
+    $l_args = $cmd->ctx()['args'];
+
+    if (count($l_args) < 3) {
+        print("Usage: calc <add|sub> <a> <b>\n");
+        exit(1);
+    }
+
+    if ($l_args[0] === "add") {
+        print($l_args[1] + $l_args[2] . "\n");
+    } else if ($l_args[0] === "sub") {
+        print($l_args[1] - $l_args[2] . "\n");
+    }
+}
