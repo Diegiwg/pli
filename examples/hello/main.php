9585663f897776f68505a4e269a75747d63e8fd2
Diegiwg <diegiwg@gmail.com>
2024-02-29 23:19:05 -0300

    test: fix the example

--- examples/hello/main.php
+++ examples/hello/main.php
@@ -2,23 +2,17 @@
 
 require_once("../../pli.php");
 
-// RUN: php main.php calc add 1 2
+$web = new Pli\Group("web");
+$web->add_command(new Pli\Command("config", fn () => print "WEB::CONFIG\n"));
+$web->add_command(new Pli\Command("build", fn () => print "WEB::BUILD\n"));
+$web->add_command(new Pli\Command("serve", fn () => print "WEB::SERVE\n"));
 
-$line = implode(" ", $argv);
-$cmd = parse_command_line($line);
 
-//
-if ($cmd->name === "calc") {
-    $l_args = $cmd->ctx()['args'];
+// MAIN //
+$app = new Pli\App();
 
-    if (count($l_args) < 3) {
-        print("Usage: calc <add|sub> <a> <b>\n");
-        exit(1);
-    }
+$app->add_group($web);
 
-    if ($l_args[0] === "add") {
-        print($l_args[1] + $l_args[2] . "\n");
-    } else if ($l_args[0] === "sub") {
-        print($l_args[1] - $l_args[2] . "\n");
-    }
-}
+$app->add_command(new Pli\Command("other", fn () => print "DEFAULT::OTHER\n"));
+
+$app->run($argc, $argv);
