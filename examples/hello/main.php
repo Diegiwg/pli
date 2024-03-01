4cd81397a41089a3fdcf01e17bd34ee463927d7b
Diegiwg <diegiwg@gmail.com>
2024-03-01 00:06:39 -0300

    test: fix example

--- examples/hello/main.php
+++ examples/hello/main.php
@@ -2,17 +2,17 @@
 
 require_once("../../pli.php");
 
-$web = new Pli\Group("web");
-$web->add_command(new Pli\Command("config", fn () => print "WEB::CONFIG\n"));
-$web->add_command(new Pli\Command("build", fn () => print "WEB::BUILD\n"));
-$web->add_command(new Pli\Command("serve", fn () => print "WEB::SERVE\n"));
+$web = new Pli\Group("web", "Web Server");
+$web->add_command(new Pli\Command("config", "Configure the web server", fn () => print "WEB::CONFIG\n"));
+$web->add_command(new Pli\Command("build", "Build the web server", fn () => print "WEB::BUILD\n"));
+$web->add_command(new Pli\Command("serve", "Serve the web server", fn () => print "WEB::SERVE\n"));
 
 
 // MAIN //
-$app = new Pli\App();
+$app = new Pli\App("Pli Example");
 
 $app->add_group($web);
 
-$app->add_command(new Pli\Command("other", fn () => print "DEFAULT::OTHER\n"));
+$app->add_command(new Pli\Command("other", "Other command", fn () => print "DEFAULT::OTHER\n"));
 
 $app->run($argc, $argv);
